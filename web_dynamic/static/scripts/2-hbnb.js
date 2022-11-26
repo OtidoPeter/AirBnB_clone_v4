bf608c092a1dbef0393624e420442893512e2274
TFrankie55 <towoeye50@gmail.com>
2022-11-25 19:15:54 -0800

    updated 2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$('document').ready(function () {
+  const url = 'http://' + window.location.hostname + ':5000/api/v1/status/';
+  $.get(url, function (response) {
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  })
+})
+
+const storeAmenity = Array();
+
+$(function () {
+  const getCheckbox = $('checkbox');
+  getCheckbox.click(function () {
+    const $this = $(this);
+    if ($this.is(':checked')) {
+      storeAmenity.push($(':amenity_id'));
+    } else { storeAmenity.pop($(':amenity_id')); }
+  });
+});
+
+const getDiv = $('.amenities#h4');
+
+// getDiv.
