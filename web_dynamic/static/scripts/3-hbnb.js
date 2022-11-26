51f175cc56ec258fefe9162b32caeca837a977a4
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-11-26 04:43:41 +0100

    added the POST method

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -9,7 +9,45 @@ $('document').ready(function () {
   })
 })
 
-$.ajax()
+$.ajax({
+  url: api + ':5001/api/v1/places_search/',
+  type: 'POST',
+  data: '{}',
+  contentType: 'application/json',
+  dataType: 'json',
+  success: function (data) {
+    $('SECTION.places').append(data.map(place => {
+      return `<ARTICLE>
+                <DIV class="title">
+                  <H2>${place.name}</H2>
+                  <DIV class="price_by_night">
+                    ${place.price_by_night}
+                  </DIV>
+                </DIV>
+                <DIV class="information">
+                  <DIV class="max_guest">
+                    <I class="fa fa-users fa-3x" aria-hidden="true"></I>
+                    </BR>
+                    ${place.max_guest} Guests
+                  </DIV>
+                  <DIV class="number_rooms">
+                    <I class="fa fa-bed fa-3x" aria-hidden="true"></I>
+                    </BR>
+                    ${place.number_rooms} Bedrooms
+                  </DIV>
+                  <DIV class="number_bathrooms">
+                    <I class="fa fa-bath fa-3x" aria-hidden="true"></I>
+                    </BR>
+                    ${place.number_bathrooms} Bathrooms
+                  </DIV>
+                </DIV>
+                <DIV class="description">
+                  ${place.description}
+                </DIV>
+              </ARTICLE>`;
+    }));
+  }
+});
 
 const storeAmenity = Array();
 
